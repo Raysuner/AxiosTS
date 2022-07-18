0a4e343bcfb3cf144e07be7cbc6c48cb6ea3242b
itnd <1669232805@qq.com>
2022-07-18 22:02:37 +0800

    feat: 测试

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-export function foo(a: number, b: number): number {
-  return a + b;
+import { AxiosRequestConfig } from "./types/axios";
+import { buildUrl } from "./utils/buildUrl";
+
+function processConfig(config: AxiosRequestConfig) {
+  const { url, params } = config;
+  const newConfig = {
+    ...config,
+    url: buildUrl(url, params),
+  };
+  return newConfig;
 }
 
-export function bar(a: number, b: number): number {
-  return a - b;
+export default function axios(config: AxiosRequestConfig) {
+  const newConfig = processConfig(config);
+  const { url = "", params = null, data = null, method = "get" } = newConfig;
+  const xhr = new XMLHttpRequest();
+  xhr.open(method.toUpperCase(), url, true);
+  xhr.send(data || params);
 }
---
+++ src/types/axios.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | "get"
+  | "GET"
+  | "post"
+  | "POST"
+  | "put"
+  | "PUT"
+  | "patch"
+  | "PATCH"
+  | "head"
+  | "HEAD"
+  | "delete"
+  | "DELETE"
+  | "options"
+  | "OPTIONS";
+
+export interface AxiosRequestConfig {
+  url: string;
+  method?: Method;
+  params?: any;
+  data?: any;
+}
