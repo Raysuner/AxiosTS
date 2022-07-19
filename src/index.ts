49296b7b3b148fca6efaabd92caae965d9474f3f
itnd <1669232805@qq.com>
2022-07-19 22:29:41 +0800

    feat: 增加携带body数据的能力

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import { AxiosRequestConfig } from "./types/axios";
 import { buildUrl } from "./utils/buildUrl";
+import { transformRequestData } from "./utils/data";
 
 function processConfig(config: AxiosRequestConfig) {
-  const { url, params } = config;
+  const { url, params, data } = config;
   const newConfig = {
     ...config,
     url: buildUrl(url, params),
+    data: transformRequestData(data),
   };
   return newConfig;
 }
 
 export default function axios(config: AxiosRequestConfig) {
   const newConfig = processConfig(config);
-  const { url = "", params = null, data = null, method = "get" } = newConfig;
+  const { url = "", data = null, method = "get" } = newConfig;
   const xhr = new XMLHttpRequest();
   xhr.open(method.toUpperCase(), url, true);
-  xhr.send(data || params);
+  xhr.send(data);
 }
