fe8081944e04d5453d63f19cc9c5c4f593ca6b55
itnd <1669232805@qq.com>
2022-08-13 12:07:35 +0800

    fix: 修改extend继承属性

--- src/utils/extend.ts
+++ src/utils/extend.ts
@@ -1,19 +1,7 @@
-const MethodList = [
-  '_request',
-  'request',
-  'get',
-  'post',
-  'head',
-  'put',
-  'patch',
-  'options',
-  'delete'
-];
-
 function extend<T, D>(to: T, from: D): T & D {
-  MethodList.forEach((key) => {
-    to[key] = from[key];
-  });
+  for (const key in from) {
+    to[key as string] = from[key];
+  }
   return to as T & D;
 }
 
