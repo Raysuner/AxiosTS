ad47df9a8bafd7aaa60c10da7e22a26ebfb6bbe1
itnd <1669232805@qq.com>
2022-08-14 18:42:48 +0800

    feat: 初始化Axios对象时带上初始配置

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import { AxiosInstance } from './types/axios';
+import { AxiosInstance, AxiosRequestConfig } from './types/axios';
+import defaults from './core/defaults';
 import extend from './utils/extend';
 import Axios from './core/axios';
 
-function createAxios(): AxiosInstance {
-  const initialAxios = new Axios();
+function createAxios(defaultConfig: AxiosRequestConfig): AxiosInstance {
+  const initialAxios = new Axios(defaultConfig);
   const instance = Axios.prototype.request.bind(initialAxios);
   extend(instance, initialAxios);
   return instance as AxiosInstance;
 }
 
-const axios = createAxios();
+const axios = createAxios(defaults);
 
 export default axios;
