f78c25e173d89b00c3f63b9ab1de02dc59d09a2c
itnd <1669232805@qq.com>
2022-08-14 18:43:23 +0800

    feat: 增加深层merge的工具函数

---
+++ src/utils/deepMerge.ts
@@ -0,0 +1,20 @@
+import { isPlainObject } from './type';
+
+export default function deepMerge(...objList: any[]): any {
+  const mergeResult = Object.create(null);
+  objList.forEach((obj) => {
+    Object.keys(obj).forEach((key) => {
+      const value = obj[key];
+      if (isPlainObject(value)) {
+        if (mergeResult[key]) {
+          mergeResult[key] = deepMerge(mergeResult[key], value);
+        } else {
+          mergeResult[key] = deepMerge({}, value);
+        }
+      } else {
+        mergeResult[key] = value;
+      }
+    });
+  });
+  return mergeResult;
+}
