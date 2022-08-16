e489698b3e5850416b4aff41dae755bbbe0ce009
itnd <1669232805@qq.com>
2022-08-16 23:05:13 +0800

    feat: 应用transfrom函数

--- src/core/request.ts
+++ src/core/request.ts
@@ -1,28 +1,24 @@
 import { AxiosPromise, AxiosRequestConfig } from '../types/axios';
 import { transformRequestUrl } from '../utils/url';
-import { transformRequestData, transformResponseData } from '../utils/data';
-import { transformRequestHeaders } from '../utils/header';
 import flattenHeaders from '../utils/flattenHeaders';
 import { xhrRequest } from './xhr';
+import transform from './transform';
 
 function processConfig(config: AxiosRequestConfig) {
-  const { url, params, data, headers = {} } = config;
+  const { url, method, params, data, transformRequest } = config;
   const newConfig: AxiosRequestConfig = {
-    ...config,
-    url: transformRequestUrl(url!, params),
-    headers: flattenHeaders(
-      transformRequestHeaders(headers, data),
-      config.method!
-    ),
-    data: transformRequestData(data)
+    ...config
   };
+  newConfig.url = transformRequestUrl(url!, params);
+  newConfig.data = transform(data, transformRequest!);
+  newConfig.headers = flattenHeaders(newConfig.headers!, method!);
   return newConfig;
 }
 
 async function request<R, P>(config: AxiosRequestConfig<P>): AxiosPromise<R> {
   const newConfig = processConfig(config);
   const res = await xhrRequest<R, P>(newConfig);
-  return transformResponseData(res);
+  return transform(res, newConfig.transformResponse!);
 }
 
 export default request;
